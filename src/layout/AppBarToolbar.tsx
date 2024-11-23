03d36a2efc88f4194d50a4b7d089c5f6f63fb5e2
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-11-23 23:32:02 +0800

    feat: fixed import error

--- src/layout/AppBarToolbar.tsx
+++ src/layout/AppBarToolbar.tsx
@@ -3,7 +3,6 @@ import {LoadingIndicator, LocalesMenuButton} from 'react-admin';
 import {ThemeSwapper} from '../themes/ThemeSwapper';
 import MyAppMenu from "./MyAppBar";
 import MyAppBadge from "./MyBadge";
-import RightBarButton from "./RightBarButton";
 import MerchantSelect from "../common/MerchantSelect";
 
 export const AppBarToolbar = () => (
