e2522618eff97871406b16f25ebfafa3f2d66624
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2025-11-20 03:00:52 +0800

    feat: fixed pick way

--- src/pages/home/Components/MyCart.tsx
+++ src/pages/home/Components/MyCart.tsx
@@ -611,8 +611,8 @@ export default function MyCart({cartItems, setCartItems, comboGroup}: MyCartProp
                         gap: 2
                     }}>
                         <FormControlLabel value={0} control={<Radio/>} label="自提"/>
-                        <FormControlLabel value={1} control={<Radio/>} label="堂食"/>
-                        <FormControlLabel value={2} control={<Radio/>} label="外卖"/>
+                        <FormControlLabel value={1} control={<Radio/>} label="外卖"/>
+                        <FormControlLabel value={2} control={<Radio/>} label="堂食"/>
                     </Box>
                 </RadioGroup>
             </FormControl>
